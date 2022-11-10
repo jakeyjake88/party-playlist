dab94d836d438594c72c84c821feeefee3737f59
Jake <jakepricep99@gmail.com>
2022-11-10 11:41:01 -0600

    doing some work in admin.js

--- web/src/pages/admin.js
+++ web/src/pages/admin.js
@@ -1,12 +1,43 @@
 import BindingClass from '../util/bindingClass';
 import DataStore from '../util/DataStore';
 import Header from '../components/header';
+import PartyPlaylistClient from '../api/partyPlaylist';
 
 class CreatePlaylist extends BindingClass {
-    constructor();
-    super();
-    //this.bindClassMethods(['mount, submit', redirectToAdmin'], this);
-    //this.dataStore = new DataStore();
-    //this.dataStore.addChangeListener(this.redirectToAdmin);
-    //this.header = new Header(this.dataStore);
+    constructor(){
+        super();
+        this.bindClassMethods(['mount', 'submit', 'redirectToAdmin']);
+        this.dataStore = new DataStore();
+        this.dataStore.addChangeListener(this.redirectToAdmin);
+        this.header = new Header(this.dataStore);
+    }
+
+    mount() {
+        document.getElementById('create').addEventListener('click', this.submit());
+        this.header.addHeaderToPage();
+        this.header.loadData();
+        this.client = new PartyPlaylistClient();
+    }
+
+    async submit() {
+        document.getElementById('create').innerText = 'Doing stuff..';
+        const playlistName = document.getElementById('aname').value;
+        const user = this.dataStore.get('aname');
+        const playlist = await this.client.createPlaylist(playlistName, user);
+        this.dataStore.set('playlist', playlist);
+        document.getElementById('create').innerText = 'Create';
+    }
+
+    redirectToAdmin() {
+        const playlist = this.dataStore.get('playlist');
+        if (playlist != null) {
+            window.location.href = `/admin.html`;
+        }
+    }
 }
+
+    const main = async () => {
+        const createPlaylist = new CreatePlaylist();
+        createPlaylist.mount();
+    }
+    window.addEventListener('DOMContentLoaded', main);
