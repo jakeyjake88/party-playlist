6278e5c6054db1f150d7ec46b922a1af1f84615e
Jake <jakepricep99@gmail.com>
2022-11-10 14:46:02 -0600

    moving some stuff around, fixing things

--- web/src/pages/admin.js
+++ web/src/pages/admin.js
@@ -1,39 +1,44 @@
 import BindingClass from '../util/bindingClass';
 import DataStore from '../util/DataStore';
 import Header from '../components/header';
-import PartyPlaylistClient from '../api/partyPlaylist';
+import PartyPlaylistClient from '../api/partyPlaylistClient';
 
 class CreatePlaylist extends BindingClass {
     constructor(){
         super();
-        this.bindClassMethods(['mount', 'submit', 'redirectToAdmin']);
+        this.bindClassMethods(['mount', 'submit', 'redirectToAdmin'], this);
         this.dataStore = new DataStore();
         this.dataStore.addChangeListener(this.redirectToAdmin);
         this.header = new Header(this.dataStore);
     }
 
     mount() {
-        document.getElementById('create').addEventListener('click', this.submit());
+        document.getElementById('create').addEventListener('click', this.submit);
         this.header.addHeaderToPage();
         this.header.loadData();
         this.client = new PartyPlaylistClient();
     }
 
     async submit() {
+        console.log("Hurrdurr");
         document.getElementById('create').innerText = 'Doing stuff..';
-        const playlistName = document.getElementById('aname').value;
-        const user = this.dataStore.get('aname');
+        const playlistName = "PlaylistName";
+        console.log(playlistName);
+        const user = "User";
+        console.log(user);
         const playlist = await this.client.createPlaylist(playlistName, user);
         this.dataStore.set('playlist', playlist);
         document.getElementById('create').innerText = 'Create';
     }
 
     redirectToAdmin() {
+        console.log("redirect to admin");
         const playlist = this.dataStore.get('playlist');
         if (playlist != null) {
             window.location.href = `/admin.html`;
         }
-    }
+    } 
+
 }
 
     const main = async () => {
