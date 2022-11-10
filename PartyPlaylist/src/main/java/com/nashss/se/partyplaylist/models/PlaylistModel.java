42f7023e08f15028de150852648c1a82f069233d
Travers Geoffray <traversgeoffray@gmail.com>
2022-11-10 13:46:52 -0600

    save before switching branches

--- PartyPlaylist/src/main/java/com/nashss/se/partyplaylist/models/PlaylistModel.java
+++ PartyPlaylist/src/main/java/com/nashss/se/partyplaylist/models/PlaylistModel.java
@@ -1,4 +1,8 @@
 package com.nashss.se.partyplaylist.models;
 
 public class PlaylistModel {
+
+    private String playlistID;
+    private String playlistName;
+    private List<PlaylistEntry> songs;
 }
