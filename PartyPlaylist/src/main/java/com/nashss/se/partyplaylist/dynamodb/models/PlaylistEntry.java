1df79668ca9c28bbb7bb24267485f863967720ee
Travers Geoffray <traversgeoffray@gmail.com>
2022-11-14 14:37:36 -0600

    created PlaylistEntry class

--- PartyPlaylist/src/main/java/com/nashss/se/partyplaylist/dynamodb/models/PlaylistEntry.java
+++ PartyPlaylist/src/main/java/com/nashss/se/partyplaylist/dynamodb/models/PlaylistEntry.java
@@ -1,4 +1,68 @@
 package com.nashss.se.partyplaylist.dynamodb.models;
 
+import com.nashss.se.partyplaylist.models.SongModel;
+
+import java.util.Objects;
+
 public class PlaylistEntry {
+
+    private final String songId;
+    private final String songTitle;
+    private final String songArtist;
+    private final String genre;
+    private final Integer songLength;
+
+    private Boolean hasPlayed;
+
+    private Integer upvotes;
+
+    public PlaylistEntry(SongModel songModel) {
+        this.songId = songModel.getSongId();
+        this.songTitle = songModel.getSongTitle();
+        this.songArtist = songModel.getSongArtist();
+        this.genre = songModel.getGenre();
+        this.songLength = songModel.getSongLength();
+    }
+
+    public Boolean getHasPlayed() {
+        return hasPlayed;
+    }
+
+    public void setHasPlayed(Boolean hasPlayed) {
+        this.hasPlayed = hasPlayed;
+    }
+
+    public Integer getUpvotes() {
+        return upvotes;
+    }
+
+    public void setUpvotes(Integer upvotes) {
+        this.upvotes = upvotes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PlaylistEntry that = (PlaylistEntry) o;
+        return Objects.equals(songId, that.songId) && Objects.equals(songTitle, that.songTitle) && Objects.equals(songArtist, that.songArtist) && Objects.equals(genre, that.genre) && Objects.equals(songLength, that.songLength);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(songId, songTitle, songArtist, genre, songLength);
+    }
+
+    @Override
+    public String toString() {
+        return "PlaylistEntry{" +
+                "songId='" + songId + '\'' +
+                ", songTitle='" + songTitle + '\'' +
+                ", songArtist='" + songArtist + '\'' +
+                ", genre='" + genre + '\'' +
+                ", songLength=" + songLength +
+                ", hasPlayed=" + hasPlayed +
+                ", upvotes=" + upvotes +
+                '}';
+    }
 }
--- PartyPlaylist/src/test/java/com/nashss/se/partyplaylist/test/helper/PlaylistTestHelper.java
+++ PartyPlaylist/src/test/java/com/nashss/se/partyplaylist/test/helper/PlaylistTestHelper.java
@@ -2,17 +2,22 @@ package com.nashss.se.partyplaylist.test.helper;
 
 import com.nashss.se.partyplaylist.dynamodb.models.Playlist;
 import com.nashss.se.partyplaylist.dynamodb.models.PlaylistEntry;
+import com.nashss.se.partyplaylist.models.SongModel;
 
 import java.util.List;
 
 public final class PlaylistTestHelper {
     private PlaylistTestHelper() {}
 
+    private static SongModel songModel = SongModel.builder()
+            .withSongId("01")
+            .build();
+
     public static Playlist generatePlaylist() {
         Playlist playlist = new Playlist();
         playlist.setPlaylistId("playlistId");
         playlist.setPlaylistName("playlist");
-        playlist.setSongs(List.of(new PlaylistEntry(), new PlaylistEntry()));
+        playlist.setSongs(List.of(new PlaylistEntry(songModel)));
 
         return playlist;
     }
