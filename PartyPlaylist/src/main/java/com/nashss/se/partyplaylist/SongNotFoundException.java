fd1101c38c2f4dce25c4e6b4d440bb08a39a0770
Travers Geoffray <traversgeoffray@gmail.com>
2022-11-14 15:18:32 -0600

    created SongDAO and SongNotFoundException

---
+++ PartyPlaylist/src/main/java/com/nashss/se/partyplaylist/SongNotFoundException.java
@@ -0,0 +1,42 @@
+package com.nashss.se.partyplaylist;
+
+/**
+ * Exception to throw when a given AlbumTrack ASIN and track number is not found
+ * in the database.
+ */
+public class SongNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = -3410734954584676544L;
+
+    /**
+     * Exception with no message or cause.
+     */
+    public SongNotFoundException() {
+        super();
+    }
+
+    /**
+     * Exception with a message, but no cause.
+     * @param message A descriptive message for this exception.
+     */
+    public SongNotFoundException(String message) {
+        super(message);
+    }
+
+    /**
+     * Exception with no message, but with a cause.
+     * @param cause The original throwable resulting in this exception.
+     */
+    public SongNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Exception with message and cause.
+     * @param message A descriptive message for this exception.
+     * @param cause The original throwable resulting in this exception.
+     */
+    public SongNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- PartyPlaylist/src/main/java/com/nashss/se/partyplaylist/dynamodb/SongDAO.java
+++ PartyPlaylist/src/main/java/com/nashss/se/partyplaylist/dynamodb/SongDAO.java
@@ -1,4 +1,45 @@
 package com.nashss.se.partyplaylist.dynamodb;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.nashss.se.partyplaylist.SongNotFoundException;
+import com.nashss.se.partyplaylist.dynamodb.models.Song;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Accesses data for a song using {@link Song} to represent the model in DynamoDB.
+ */
+@Singleton
 public class SongDAO {
+
+    private final DynamoDBMapper dynamoDbMapper;
+
+    /**
+     * Instantiates an SongDAO object.
+     *
+     * @param dynamoDbMapper the {@link DynamoDBMapper} used to interact with the songs table
+     */
+    @Inject
+    public SongDAO(DynamoDBMapper dynamoDbMapper) {
+            this.dynamoDbMapper = dynamoDbMapper;
+        }
+
+    /**
+     * Retrieves a Song by songID.
+     *
+     * If not found, throws SongNotFoundException.
+     *
+     * @param songId the songId to look up
+     * @return The corresponding Song if found
+     */
+    public Song getSong(String songId) {
+        Song song = dynamoDbMapper.load(Song.class, songId);
+        if (null == song) {
+            throw new SongNotFoundException(
+                    String.format("Could not find Song with songId %s", songId));
+        }
+
+        return song;
+    }
 }
