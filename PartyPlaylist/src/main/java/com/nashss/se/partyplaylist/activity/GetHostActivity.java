f3d9e86bcc1360599b66ec0ceb85b31ffc909940
Travers Geoffray <traversgeoffray@gmail.com>
2022-11-29 09:43:43 -0600

    created GetHostActivity class

---
+++ PartyPlaylist/src/main/java/com/nashss/se/partyplaylist/activity/GetHostActivity.java
@@ -0,0 +1,24 @@
+package com.nashss.se.partyplaylist.activity;
+
+import com.nashss.se.partyplaylist.dynamodb.PlaylistDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Implementation of the GetGuestActivity for the PartyPlaylist GetGuest API.
+ *
+ * This API allows the host to get one of the guests at the party.
+ */
+public class GetHostActivity {
+
+    private final Logger log = LogManager.getLogger();
+
+    private final PlaylistDao playlistDao;
+
+    /**
+     * Instantiates a new GetHostActivity object.
+     *
+     * @param playlistDao to access the playlist table.
+     */
+    
+}
