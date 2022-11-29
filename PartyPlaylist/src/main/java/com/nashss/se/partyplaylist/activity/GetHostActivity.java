4125cf570395713eeab92d82b5c27bd687296ba0
Travers Geoffray <traversgeoffray@gmail.com>
2022-11-29 09:59:47 -0600

    working on getHost

--- PartyPlaylist/src/main/java/com/nashss/se/partyplaylist/activity/GetHostActivity.java
+++ PartyPlaylist/src/main/java/com/nashss/se/partyplaylist/activity/GetHostActivity.java
@@ -1,8 +1,11 @@
 package com.nashss.se.partyplaylist.activity;
 
 import com.nashss.se.partyplaylist.dynamodb.PlaylistDao;
+import com.nashss.se.partyplaylist.models.UserModel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import javax.inject.Inject;
 
 /**
  * Implementation of the GetGuestActivity for the PartyPlaylist GetGuest API.
@@ -20,5 +23,23 @@ public class GetHostActivity {
      *
      * @param playlistDao to access the playlist table.
      */
-    
+    @Inject
+    public GetHostActivity(PlaylistDao playlistDao) {
+        this.playlistDao = playlistDao;
+    }
+
+    //CHECKSTYLE:OFF:LeftCurly
+    /**
+     * This method handles the incoming request by retrieving the host from the database.
+     * <p>
+     * It then returns the host.
+     * <p>
+     * If the host does not exist, this should throw a UserNotFoundException.
+     *
+     * @param getHostRequest request object containing the userId
+     * @return getHostResult result object containing the API defined {@link UserModel }
+     */
+    public GetHostResult handleRequest(final GetHostRequest getHostRequest) {
+
+    }
 }
